f5a2534a9110754e692413dc14272bc0b8d0ee0d
U-WWW-9F6D45EF1C6\Administrator <Administrator@WWW-9F6D45EF1C6.(none)>
2013-05-07 13:16:01 +0800

    the third lesson

---
+++ register.php
@@ -0,0 +1,29 @@
+﻿<html>
+<head>
+<title>用户注册</title>
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+<script type="text/javascript">
+	
+	//创建ajax引擎
+	function getXml
+</script>
+</head>
+<body>
+	<form action="???" method="post">
+    用户名字:<input type="text"  onkeyup="checkName();"  name="username1" id="username"><input type="button" onclick="checkName();"  value="验证用户名">
+    <input style="border-width: 0;color: red" type="text" id="myres">
+    <br/>
+    用户密码:<input type="password" name="password"><br>
+    电子邮件:<input type="text" name="email"><br/>
+    <input type="submit" value="用户注册">
+    </form>
+     <form action="???" method="post">
+    用户名字:<input type="text" name="username2" >
+   
+    <br/>
+    用户密码:<input type="password" name="password"><br>
+    电子邮件:<input type="text" name="email"><br/>
+    <input type="submit" value="用户注册">
+    </form>
+</body>
+</html>
